76fe4a72aa8f002f1ccb6f60e6b032a3b29eda8c
Admin <Admin@DESKTOP-8BMTFAI>
2020-04-23 19:13:05 +0530

    First Commit

---
+++ src/main/java/com/api/rest_ssapi_helper/App.java
@@ -0,0 +1,13 @@
+package com.api.rest_ssapi_helper;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
